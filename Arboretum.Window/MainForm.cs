c3f878e65fe73c733a2cc9dd0a59da78ce9a1012
celophi <celophi@todoku.me>
2018-10-13 11:10:44 +0900

    Added a menu for extracting individual files.

--- Arboretum.Window/MainForm.cs
+++ Arboretum.Window/MainForm.cs
@@ -1,14 +1,10 @@
 ﻿using Arboretum.Lib;
 using Arboretum.Window;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Arboretum
@@ -20,6 +16,7 @@ namespace Arboretum
         public MainForm()
         {
             InitializeComponent();
+            this.ArchiveContentsTreeView.MouseUp += this.On_TreeView_MouseUp;
         }
 
         private void toolStripStatusLabel1_Click(object sender, EventArgs e)
@@ -65,48 +62,129 @@ namespace Arboretum
                 _archive = new PakZip(dialog.FileName);
                 this.tabControl1.SelectedTab = this.ArchiveContentsTab;
                 this.toolStripStatusLabel1.Text = dialog.FileName;
-                this.PopulateArchiveTree();
+
+                var paths = _archive.PakFiles.Select(x => x.FileName);
+                foreach (var path in paths)
+                {
+                    this.AttachPathToTree(this.ArchiveContentsTreeView, path);
+                }
             }
         }
 
         /// <summary>
-        /// Populates the archive view tree.
+        /// Attaches a file path to the tree.
         /// </summary>
-        private void PopulateArchiveTree()
+        /// <param name="tree"></param>
+        /// <param name="path"></param>
+        private void AttachPathToTree(TreeView tree, string path)
         {
-            TreeNode lastNode = null;
-            string subPathAgg;
+            var key = String.Empty;
+            TreeNode last = null;
+            var partials = path.Split(new[] { Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar });
 
-            // TODO: Find a better path separator.
-            char pathSeparator = '/';
-
-            this.ArchiveContentsTreeView.Nodes.Clear();
-            var paths = _archive.PakFiles.Select(x => x.FileName);
-            foreach (string path in paths)
+            foreach (var partial in partials)
             {
-                subPathAgg = string.Empty;
-                foreach (string subPath in path.Split(pathSeparator))
+                key += (partial + '/');
+
+                var nodes = tree.Nodes.Find(key, true);
+                if (nodes.Length > 0)
                 {
-                    subPathAgg += subPath + pathSeparator;
-                    TreeNode[] nodes = this.ArchiveContentsTreeView.Nodes.Find(subPathAgg, true);
-
-                    if (nodes.Length > 0)
-                    {
-                        lastNode = nodes[0];
-                        continue;
-                    }
-
-                    if (lastNode == null)
-                    {
-                        lastNode = this.ArchiveContentsTreeView.Nodes.Add(subPathAgg, subPath);
-                        continue;
-                    }
-
-                    lastNode = lastNode.Nodes.Add(subPathAgg, subPath);
+                    last = nodes[0];
+                    continue;
                 }
 
-                lastNode = null;
+                var node = new TreeNode(partial);
+                node.Name = key;
+
+                // If the key and path are the same, attach the path as metadata.
+                // This can be any metadata object.
+                if (key.Contains(path))
+                {
+                    this.AttachDataToEndNode(node, path);
+                }
+
+                if (last == null)
+                {
+                    last = node;
+                    tree.Nodes.Add(node);
+                    continue;
+                }
+
+                last.Nodes.Add(node);
+                last = node;
             }
+        }
+
+        /// <summary>
+        /// Attaches a context menu and metadata to a tree node.
+        /// </summary>
+        /// <param name="node"></param>
+        /// <param name="path"></param>
+        private void AttachDataToEndNode(TreeNode node, string path)
+        {
+            node.Tag = path;
+
+            var menu = new ContextMenuStrip();
+            var menuItem = new ToolStripMenuItem
+            {
+                Text = "Extract",
+                Tag = node
+            };
+            menu.ItemClicked += new ToolStripItemClickedEventHandler(this.On_ContextMenu_ItemClicked);
+            menu.Items.Add(menuItem);
+
+            node.ContextMenuStrip = menu;
+        }
+        
+        /// <summary>
+        /// Handler to perform extraction when menu item clicked.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void On_ContextMenu_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            ToolStripItem item = e.ClickedItem;
+            if (item.Text == "Extract")
+            {
+                var node = this.ArchiveContentsTreeView.SelectedNode;
+                var path = (string)node.Tag;
+                
+                var dlg = new FolderBrowserDialog();
+                dlg.ShowNewFolderButton = true;
+                dlg.Description = "Extract the selected file to this location.";
+                var result = dlg.ShowDialog();
+
+                if (result == DialogResult.OK)
+                {
+                    var outputDirectory = dlg.SelectedPath;
+                    _archive.Extract(path, outputDirectory);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Handler for selecting the clicked node in a tree.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void On_TreeView_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button != MouseButtons.Right)
+            {
+                return;
+            }
+
+            var pt = new Point(e.X, e.Y);
+            var tree = (TreeView)sender;
+            tree.PointToClient(pt);
+
+            var node = tree.GetNodeAt(pt);
+            if (node == null)
+            {
+                return;
+            }
+
+            tree.SelectedNode = node;
         }
     }
 }
